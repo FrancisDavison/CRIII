d19ce28074daab3929f5cd1a496ef3d3456539a9
FrancisDavison <francis.nj.davison@gmail.com>
2021-02-15 15:17:10 +0000

    Updated Question 1 code

--- CRIII/src/Question_1.java
+++ CRIII/src/Question_1.java
@@ -1,34 +1,50 @@
 import java.util.ArrayList;
-
 public class Question_1
 {
 	public static void main(String args[])
 	{
 		ArrayList<Boolean> rep = new ArrayList<Boolean>();
 		rep.add(true);
+		rep.add(false);
+		rep.add(true);
+		rep.add(false);
+		rep.add(true);
+		rep.add(false);
 		ArrayList<Double> weights = new ArrayList<Double>();
-		weights.add(15.0);
+		weights.add(10.0);
+		weights.add(150.0);
+		weights.add(2.0);
+		weights.add(1.0);
+		weights.add(150.0);
+		weights.add(29.0);
 		ScalesFitness(rep, weights);
 	}
 	
 	public static double ScalesFitness(ArrayList<Boolean> rep, ArrayList<Double> weights)
 	{
-		double out = -100;
-		if(rep == null)
+
+		if (rep==null||weights==null) {
+			return -100;
+		} 
+		
+		if(rep.size()==0||weights.size()==0||rep.size()>weights.size())
 		{
-			return out;
+			return -100;
 		}
-		if(weights == null)
-		{
-			return out;
-		}
-		if(rep.size()==0||weights.size()==0)
-		{
-			return out;
-		}
-		if(rep.size()>weights.size())
-		{
-			return -out;
-		}
+			double lhs=0.0, rhs=0.0;
+			int n=rep.size();
+
+			for (int i=0;i<n;i++)
+			{
+				if (rep.get(i)==true)
+				{
+					lhs=lhs+weights.get(i);
+				} 
+				else
+				{
+					rhs=rhs+weights.get(i);
+				}
+			}
+			return Math.abs(lhs-rhs);
 	}
 }
